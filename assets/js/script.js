bee2a437ed8c57bb12049ed6e1e7ac3e5b522573
Matadon27 <mexanoid2012@gmail.com>
2023-08-17 03:06:46 +0300

    17.08.2023 3:07

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-let myPage = document.querySelector('body');
+let categories = document.querySelectorAll('.categoryName');
+
+categories[0].childNodes
+
+document.addEventListener('click', e => {
+    if (!e.target.classList.contains('categoryName')) return;
+    e.target.nextElementSibling.classList.toggle('collapse');
+    e.target.children[0].classList.toggle('open');
+})
